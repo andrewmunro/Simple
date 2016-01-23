1158f18ce2f59a1fb9203c098d445aeb11008b41
Andrew <synestry@gmail.com>
2016-01-23 01:07:57 +0000

    Adding multiplayer vehicles

---
+++ Assets/Scripts/Entity/VehicleEntity.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+using UnityStandardAssets.Vehicles.Car;
+
+namespace Assets.Scripts.Entity
+{
+    [RequireComponent(typeof(CarUserControl))]
+    [RequireComponent(typeof(CarController))]
+    public class VehicleEntity : AbstractEntity
+    {
+        public CarController Controller { get; private set; }
+        public CarUserControl CarUserControl { get; private set; }
+        public Camera Camera { get; private set; }
+
+        public override void OnStartClient()
+        {
+            base.OnStartClient();
+
+            Controller = GetComponent<CarController>();
+            CarUserControl = GetComponent<CarUserControl>();
+            Camera = gameObject.GetComponentInChildren<Camera>();
+            SetPlayerState(false);
+        }
+
+        // Use this for initialization
+        public override void OnStartAuthority()
+        {
+            base.OnStartAuthority();
+
+            SetPlayerState(true);
+        }
+
+        public override void OnStopAuthority()
+        {
+            base.OnStopAuthority();
+
+            SetPlayerState(false);
+        }
+
+        private void SetPlayerState(bool local)
+        {
+            Controller.enabled = local;
+            CarUserControl.enabled = local;
+            Camera.gameObject.SetActive(local);
+        }
+    }
+}
--- Assets/Standard Assets/Vehicles/Car/Scripts/WheelEffects.cs
+++ Assets/Standard Assets/Vehicles/Car/Scripts/WheelEffects.cs
@@ -40,8 +40,8 @@ namespace UnityStandardAssets.Vehicles.Car
 
         public void EmitTyreSmoke()
         {
-            skidParticles.transform.position = transform.position - transform.up*m_WheelCollider.radius;
-            skidParticles.Emit(1);
+//            skidParticles.transform.position = transform.position - transform.up*m_WheelCollider.radius;
+//            skidParticles.Emit(1);
             if (!skidding)
             {
                 StartCoroutine(StartSkidTrail());
